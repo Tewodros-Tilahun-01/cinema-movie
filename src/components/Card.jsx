46eae23a8ff3c746e3acc6da6f57cc80c12edb9e
teddy <teddytilahun02@gmail.com>
2024-06-05 23:24:45 +0300

    fix typo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import "./card.css"
+import React from "react";
+import "./card.css";
 function Card({ movie }) {
     return (
         <div className="col-lg-2 col-md-4 sol-sm-6">
             <div className="movie-card">
-                <img src={movie.previewImg} alt="prviewImg" className='img-fluid' />
-                <p>{movie.length} | {movie.catagory}</p>
+                <img
+                    src={movie.previewImg}
+                    alt="prviewImg"
+                    className="img-fluid"
+                />
+                <p>{movie.length} </p>
                 <div className="content">
-                    <h4>
-                        {movie.title}
-                    </h4>
+                    <h4>{movie.title}</h4>
                     <div className="card-icons">
                         <ion-icon name="add-outline"></ion-icon>
                         <ion-icon name="play-outline"></ion-icon>
                     </div>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Card
+export default Card;
