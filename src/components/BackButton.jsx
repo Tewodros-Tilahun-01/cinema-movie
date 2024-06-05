0d76a6c62ec2de5533daae492a14b0786505c4dc
teddy <teddytilahun02@gmail.com>
2024-06-05 16:42:24 +0300

    change typo

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import "./backbutton.css"
+import React from "react";
+import "./backbutton.css";
 function BackButton(props) {
     return (
-        <button className='back-button' onClick={() => props.setSearchOn(false)}>Back</button>
-    )
+        <button
+            className="back-button"
+            onClick={() => props.setSearchOn(false)}
+        >
+            x
+        </button>
+    );
 }
 
-export default BackButton
+export default BackButton;
